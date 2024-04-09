d2836f069e0717452c6d682cf8f72da4becfa69a
youngjean <rateenyj@naver.com>
2024-04-10 01:38:31 +0900

    [batch] Spring Batch 스키마 자동 생성 설정 #2

--- src/main/java/io/springbatch/springbatchlecture/DBJobConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/DBJobConfiguration.java
@@ -1,10 +1,18 @@
 package io.springbatch.springbatchlecture;
 
-import io.micrometer.core.instrument.util.JsonUtils;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor
 @Configuration
 public class DBJobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+
+
 }
